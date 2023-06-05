f95adcd678a2edeb0d1178c01a5829291f5e5197
nobruzeiraaa <116617881+nobruzeiraaa@users.noreply.github.com>
2023-06-05 14:00:51 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,42 @@
+package prjtCalculadora;
+import java.util.Scanner;
+public class App {
+
+	public static void main(String[] args) {
+		
+		Calculadora calc = new Calculadora();
+		Scanner in = new Scanner(System.in);
+		
+		int operacao;
+		
+		System.out.println("Digite o Primeiro Número: "); 
+		calc.numero1 = in.nextInt();
+		
+		System.out.println("Digite o Segundo Número" );
+		calc.numero2 = in.nextInt();
+		
+		System.out.print("\nDigite 1 para + \nDigite 2 para - \nDigite 3 para ÷ \nDigite 4 para x \n" );
+		operacao = in.nextInt();
+		
+		switch (operacao) {
+		case 1:
+			calc.somar();
+			break;
+		case 2:
+			calc.subtrair();
+			break;
+		case 3:
+			calc.dividir();
+			break;
+		case 4:
+			calc.multiplicar();
+			break;
+			default:
+				System.out.println("Operador inválido");
+		}
+		
+		System.out.println(calc.resultado);
+
+	}
+
+}
---
+++ Calculadora.java
@@ -0,0 +1,24 @@
+package prjtCalculadora;
+
+public class Calculadora {
+	
+	double numero1;
+	double numero2;
+	double resultado;	
+	
+	public void somar() {
+		resultado = numero1 + numero2;
+	}
+	public void dividir() {
+		resultado = numero1 / numero2; 
+	}
+	public void subtrair() {
+		resultado = numero1 - numero2;
+	}
+	public void multiplicar() {
+		resultado = numero1 * numero2;
+		
+	
+		
+	}
+}
